69181115f7018290c90dfaa73d0b4fbd9ade1c31
ivannofick@gmail.com <ivannofick.nugraha@mncgroup.com>
2023-09-05 23:09:26 +0700

    feat: add template homepage

---
+++ resources/views/default.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Gemes</title>
+  </head>
+  <body>
+    @yield('contents')
+  </body>
+</html>
