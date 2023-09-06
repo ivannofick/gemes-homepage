8098f0dc7ec015fda089a81ccb4d14aefdb8031c
ivannofick <ivannofick.nugraha@mncgroup.com>
2023-09-06 17:56:47 +0700

    feat:fixed spacing

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,34 +1,35 @@
 @extends('default')
 @section('contents')
-<style>
-    .swiper-slide { width: 80% !important; }
-</style>
+    <style>
+        .swiper-slide {
+            width: 80% !important;
+        }
+    </style>
     <div class="swiper" id="mySwiper1">
-    <div class="swiper mySwiper">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" width="300" />
-            </div>
-            <div class="swiper-slide">
-                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
-            </div>
-            <div class="swiper-slide">
-                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
-            </div>
-            <div class="swiper-slide">
-                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" width="300" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+                </div>
+
             </div>
 
         </div>
-
-    </div>
-    <script>
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: "auto",
-            spaceBetween: 30,
-            initialSlide: 1,
-            centeredSlides: true,
-        });
-
-    </script>
-@endsection
+        <script>
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: "auto",
+                spaceBetween: 30,
+                initialSlide: 1,
+                centeredSlides: true,
+            });
+        </script>
+    @endsection
