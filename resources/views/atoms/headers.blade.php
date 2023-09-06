22ab2cca3174c9bdf5249185764d5b86b061b45b
ivannofick <ivannofick.nugraha@mncgroup.com>
2023-09-06 11:20:43 +0700

    feat: change template

---
+++ resources/views/atoms/headers.blade.php
@@ -0,0 +1,9 @@
+<div class="border-b border-gray-200 bg-zinc-800 dark:border-zinc-600 p-4 flex items-center justify-between">
+    <div class="flex items-center">
+        <h1 class="text-white text-lg font-semibold">Nama Aplikasi Anda</h1>
+    </div>
+
+    <div class="svg-yellow">
+        <img class="h-8 w-8" alt="lonceng" src="{{ asset('/assets/img/icon/bell-icon.svg') }}">
+    </div>
+</div>
--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,17 +4,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="{{ asset('/assets/css/custom-color.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 
     <title>Gemes</title>
   </head>
-  <body>
-    <!-- Header -->
-    <header class="bg-blue-500 p-4">
-      <h1 class="text-white text-2xl text-center">Your Mobile App</h1>
-    </header>
-    <!-- Content -->
+  <body class="dark:bg-zinc-800">
+    @include('atoms.headers')
     <main class="p-4">
       @yield('contents')
     </main>
