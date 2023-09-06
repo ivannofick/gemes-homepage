4319a65c8dd65466ee718f67e6c49514589abbe1
ivannofick <ivannofick.nugraha@mncgroup.com>
2023-09-06 17:58:37 +0700

    feat: add sample 2 swipper

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -5,31 +5,53 @@
             width: 80% !important;
         }
     </style>
-    <div class="swiper" id="mySwiper1">
-        <div class="swiper mySwiper">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" width="300" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
-                </div>
-
+    <div class="swiper mb-4" id="mySwiper1">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" width="300" />
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
             </div>
 
         </div>
-        <script>
-            var swiper = new Swiper(".mySwiper", {
-                slidesPerView: "auto",
-                spaceBetween: 30,
-                initialSlide: 1,
-                centeredSlides: true,
-            });
-        </script>
-    @endsection
+    </div>
+    <div class="swiper" id="mySwiper1">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" width="300" />
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+            </div>
+            <div class="swiper-slide">
+                <img src="{{ asset('/assets/img/football.svg') }}" alt="..." class="w-full" />
+            </div>
+
+        </div>
+
+    </div>
+    <script>
+        new Swiper("#mySwiper1", {
+            slidesPerView: "auto",
+            spaceBetween: 30,
+            initialSlide: 1,
+            centeredSlides: true,
+        });
+        new Swiper("#mySwiper2", {
+            slidesPerView: "auto",
+            spaceBetween: 30,
+            initialSlide: 1,
+            centeredSlides: true,
+        });
+    </script>
+@endsection
